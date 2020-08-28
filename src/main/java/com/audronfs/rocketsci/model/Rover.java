a9580bd59f220fd508fc814ca2b04a5e30077a5d
Aurora del Rio <auroradelrio@gmail.com>
2020-08-28 07:15:46 +0100

    Defined rover constructor and new class Position

--- src/main/java/com/audronfs/rocketsci/model/Rover.java
+++ src/main/java/com/audronfs/rocketsci/model/Rover.java
@@ -1,5 +1,14 @@
 package com.audronfs.rocketsci.model;
 
+
+
 public class Rover {
+	
+public Rover(int x, int y, String orientation, Plateau plateau) {
+	
+		
+	}
+	
+	//Gettters and Setters
 
 }
---
+++ src/main/java/com/audronfs/rocketsci/util/Constants.java
@@ -0,0 +1,6 @@
+package com.audronfs.rocketsci.util;
+
+public class Constants {
+	public static final String SEPARATOR_SPLIT_CHARACTER = ",";
+
+}
